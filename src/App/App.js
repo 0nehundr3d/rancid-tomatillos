3af878340996026e2a0bc81631f5dff15a87119b
Sebastian McKee <sebmcke@gmail.com>
2025-04-03 16:22:58 -0500

    Add method to up and down vote movies

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 import searchIcon from '../icons/search.png';
 
 // Example imports (for later):
@@ -8,13 +9,29 @@ import moviePosters from '../data/movie_posters';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
 
 function App() {
-  console.log(moviePosters);
+  const [scores, setScores] = useState(moviePosters.reduce((acc, movie) => {
+    acc[movie.id] = movie.vote_count
+    return acc
+  }, {}))
+
+  function changeScore(movieId, upvote) {
+    const score = upvote ? 1 : -1
+    const movieIndex = moviePosters.findIndex(movie => movie.id === movieId)
+
+    let newScores = structuredClone(scores)
+    newScores[movieId.toString()] += score
+
+    moviePosters[movieIndex].vote_count += score
+    
+    setScores(newScores)
+  }
+
   return (
     <main className='App'>
       <header>
         <h1>rancid tomatillos</h1>
       </header>
-      <MoviesContainer moviePosters={moviePosters}/>
+      <MoviesContainer moviePosters={moviePosters} changeScore={changeScore}/>
     </main>
   );
 }
--- src/MoviesContainer/MoviesContainer.js
+++ src/MoviesContainer/MoviesContainer.js
@@ -2,15 +2,17 @@ import moviePosters from '../data/movie_posters';
 import MoviePoster from '../MoviePoster/MoviePoster';
 import './MoviesContainer.css';
 
-function MoviesContainer({ moviePosters }) { // Object destructuring {moviePosters} - saying that "you can now just go use moviePosters directly"
+function MoviesContainer({ moviePosters, changeScore }) { // Object destructuring {moviePosters} - saying that "you can now just go use moviePosters directly"
   return (
       <section className='MoviesContainer'>
         {moviePosters.map(movie => (
           <MoviePoster
+            id={movie.id}
             key={movie.id}
             title={movie.title}
             poster_path={movie.poster_path}
             vote_count={movie.vote_count}
+            changeScore={changeScore}
           />
         ))}
       </section>
