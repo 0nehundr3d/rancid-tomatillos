ad406ca400700e163384e9236474bfad7d3eb8e6
Shadeau Christensen <octogrim-mac@Shadeaus-MacBook-Air-Octogrim.local>
2025-04-05 11:29:08 -0700

    Built out MovieDetails and styled, refeactored up/down vote

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,9 +1,21 @@
 import './MovieDetails.css';
 
-function MovieDetails() {
+function MovieDetails({ movie }) {
+  const { backdrop_path, title, genre_ids, overview } = movie
+
   return (
     <section className='MovieDetails'>
-      <p>Movie Details go here!</p>
+      <img src={backdrop_path} alt={`Backdrop for ${title}`} />
+
+      <div className="DetailsBox">
+        <h2>{movie.title}</h2>
+          <div className="GenreContainer">
+            {genre_ids.map((genre, index) => (
+              <span key={index} className="GenreBox">{genre}</span>
+            ))}
+          </div>
+        <p>{movie.overview}</p>
+      </div>
     </section>
   );
 }
--- src/MoviesContainer/MoviesContainer.js
+++ src/MoviesContainer/MoviesContainer.js
@@ -2,7 +2,7 @@ import moviePosters from '../data/movie_posters';
 import MoviePoster from '../MoviePoster/MoviePoster';
 import './MoviesContainer.css';
 
-function MoviesContainer({ moviePosters, changeScore }) { // Object destructuring {moviePosters} - saying that "you can now just go use moviePosters directly"
+function MoviesContainer({ moviePosters, changeScore, showMovieDetails }) { // Object destructuring {moviePosters} - saying that "you can now just go use moviePosters directly"
   return (
       <section className='MoviesContainer'>
         {moviePosters.map(movie => (
@@ -13,10 +13,11 @@ function MoviesContainer({ moviePosters, changeScore }) { // Object destructurin
             poster_path={movie.poster_path}
             vote_count={movie.vote_count}
             changeScore={changeScore}
+            showMovieDetails={showMovieDetails}
           />
         ))}
       </section>
-  );
+  )
 }
   
 export default MoviesContainer;
